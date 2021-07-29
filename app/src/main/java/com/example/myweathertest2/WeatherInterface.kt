3ab4c14ee7cc7dea033890f6ab710f6a3941c0b4
minWachya <alal7272@gmail.com>
2021-07-29 22:20:27 +0900

    코드 정리 & baseTime 가져오기 수정

--- app/src/main/java/com/example/myweathertest2/WeatherInterface.kt
+++ app/src/main/java/com/example/myweathertest2/WeatherInterface.kt
@@ -26,7 +26,7 @@ interface WeatherInterface {
 data class WEATHER (val response : RESPONSE)
 data class RESPONSE(val header : HEADER, val body : BODY)
 data class HEADER(val resultCode : Int, val resultMsg : String)
-data class BODY(val dataType : String, val items : ITEMS)
+data class BODY(val dataType : String, val items : ITEMS, val totalCount : Int)
 data class ITEMS(val item : List<ITEM>)
 // category : 자료 구분 코드, fcstDate : 예측 날짜, fcstTime : 예측 시간, fcstValue : 예보 값
 data class ITEM(val category : String, val fcstDate : String, val fcstTime : String, val fcstValue : String)
